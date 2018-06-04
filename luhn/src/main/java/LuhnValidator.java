89d078d2e3c8fca762beec78f9b74cc27d1368db
Paul Kelly <paul.kelly20@gmail.com>
2018-06-04 09:16:06 +0100

    submitted luhn after tests passed

--- luhn/src/main/java/LuhnValidator.java
+++ luhn/src/main/java/LuhnValidator.java
@@ -1,7 +1,38 @@
+import java.util.ArrayList;
+
 class LuhnValidator {
 
     boolean isValid(String candidate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        candidate = candidate.replaceAll("\\s+","");
+        if(candidate.length() <= 1){
+            return false;
+        }
+        int sum = 0;
+        ArrayList<Integer> candidateIntArray = new ArrayList<>();
+        for (int i = 0; i <candidate.length(); i++){
+            if(Character.isDigit(candidate.charAt(i))){
+                candidateIntArray.add(Character.getNumericValue(candidate.charAt(i)));
+
+                }if(!Character.isDigit(candidate.charAt(i))){
+                return false;
+            }}
+
+        for (int i = candidateIntArray.size() -1 ; i >= 0; i -= 2)
+        { sum += candidateIntArray.get(i);}
+
+
+        for (int j = candidateIntArray.size() -2; j >= 0; j -=2){
+             if(candidateIntArray.get(j) * 2 < 10){
+                 sum += candidateIntArray.get(j) * 2;}
+                 else sum += ((candidateIntArray.get(j) * 2) -9);
+             }
+
+             if(sum % 10 == 0){return true;}
+
+             return false;
+        }
+
 
 }
+
+
--- luhn/src/test/java/LuhnValidatorTest.java
+++ luhn/src/test/java/LuhnValidatorTest.java
@@ -18,49 +18,41 @@ public class LuhnValidatorTest {
         assertFalse(luhnValidator.isValid("1"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatTheStringConsistingOfASingleZeroIsInvalid() {
         assertFalse(luhnValidator.isValid("0"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatASimpleValidNumberIsIdentifiedAsValid() {
         assertTrue(luhnValidator.isValid(" 5 9 "));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatAValidCanadianSocialInsuranceNumberIsIdentifiedAsValidV1() {
         assertTrue(luhnValidator.isValid("046 454 286"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatAValidCanadianSocialInsuranceNumberIsIdentifiedAsValidV2() {
         assertTrue(luhnValidator.isValid("055 444 285"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatAnInvalidCanadianSocialInsuranceNumberIsIdentifiedAsInvalid() {
         assertFalse(luhnValidator.isValid("046 454 287"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatAnInvalidCreditCardIsIdentifiedAsInvalid() {
         assertFalse(luhnValidator.isValid("8273 1232 7352 0569"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatAddingANonDigitCharacterToAValidStringInvalidatesTheString() {
         assertFalse(luhnValidator.isValid("046a 454 286"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatStringContainingPunctuationIsInvalid() {
         assertFalse(luhnValidator.isValid("055-444-285"));
@@ -71,25 +63,21 @@ public class LuhnValidatorTest {
      * passing implementations. For more detail, check out issue #972 here:
      * (https://github.com/exercism/java/issues/972).
     */
-    @Ignore("Remove to run test")
     @Test
     public void testThatStringContainingSymbolsIsInvalid() {
         assertFalse(luhnValidator.isValid("34&"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatTheStringConsistingOfASpaceAndASingleZeroIsInvalid() {
         assertFalse(luhnValidator.isValid(" 0"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatStringContainingMultipleZerosIsValid() {
         assertTrue(luhnValidator.isValid(" 00000"));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testThatDoublingNineIsHandledCorrectly() {
         assertTrue(luhnValidator.isValid("091"));
